56a37d08c8afed746280d35ba52b2f4ec605f6fd
Nikolay Cherkashin <nikkirche@hotmail.com>
2024-12-24 14:15:03 +0000

    fc

--- check_ctx_speed.sh
+++ check_ctx_speed.sh
@@ -1,2 +1,17 @@
-time ./build/verifying/targets/atomic_register --tasks 1000 --rounds 2000 --strategy random --threads 250 --verbose false --switches 100
-time ./build-old/verifying/targets/atomic_register --tasks 1000 --rounds 2000 --strategy random --threads 250 --verbose false --switches 100
+#!bin/bash 
+set -e
+dir=build-bench
+new=$1
+args="--tasks 1000 --rounds 2000 --strategy random --threads 250 --verbose false --switches 100"
+git switch $new
+cmake -G Ninja -B $dir -DCMAKE_BUILD_TYPE=Release 
+cmake --build $dir --target verifying/targets/atomic_register
+echo "new impl from $new branch"
+time ./$dir/verifying/targets/atomic_register $args
+git switch master
+cmake -G Ninja -B $dir -DCMAKE_BUILD_TYPE=Release >/dev/null 2>&1
+cmake --build $dir --target verifying/targets/atomic_register >/dev/null 2>&1
+echo "base impl"
+time ./$dir/verifying/targets/atomic_register $args
+rm -rf $dir
+git switch $new
