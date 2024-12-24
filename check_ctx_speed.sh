28e046241c611d636c4be54e0931ef6c2ae9f2df
Nikolay Cherkashin <nikkirche@hotmail.com>
2024-12-24 12:25:06 +0000

    clean script

--- check_ctx_speed.sh
+++ check_ctx_speed.sh
@@ -1,10 +1,2 @@
-time ./build/verifying/targets/atomic_register --tasks 1000 --rounds 2000 --strategy random
- --threads 250 --verbose false --switches 100
-# real    0m17.835s
-# user    0m17.410s
-# sys     0m0.421s
-time ./build-old/verifying/targets/atomic_register --tasks 1000 --rounds 2000 --strategy random
- --threads 250 --verbose false --switches 100
-# real    0m42.766s
-# user    0m24.256s
-# sys     0m18.501s
+time ./build/verifying/targets/atomic_register --tasks 1000 --rounds 2000 --strategy random --threads 250 --verbose false --switches 100
+time ./build-old/verifying/targets/atomic_register --tasks 1000 --rounds 2000 --strategy random --threads 250 --verbose false --switches 100
