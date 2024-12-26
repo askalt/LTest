733657518f5ad4deb9e214e571805753c6320a75
Nikolay Cherkashin <nikkirche@hotmail.com>
2024-12-26 20:55:08 +0000

    fixed logic of bench script

--- check_ctx_speed.sh
+++ check_ctx_speed.sh
@@ -1,17 +1,19 @@
 #!bin/bash 
 set -e
 dir=build-bench
-new=$1
+baseline=$1
+curr=$(git rev-parse --abbrev-ref HEAD)
 args="--tasks 1000 --rounds 2000 --strategy random --threads 250 --verbose false --switches 100"
-git switch $new
 cmake -G Ninja -B $dir -DCMAKE_BUILD_TYPE=Release 
 cmake --build $dir --target verifying/targets/atomic_register
-echo "new impl from $new branch"
+echo "impl from $curr branch"
 time ./$dir/verifying/targets/atomic_register $args
-git switch master
+git stash
+git checkout $baseline
 cmake -G Ninja -B $dir -DCMAKE_BUILD_TYPE=Release >/dev/null 2>&1
 cmake --build $dir --target verifying/targets/atomic_register >/dev/null 2>&1
-echo "base impl"
+echo "impl from $baseline commit"
 time ./$dir/verifying/targets/atomic_register $args
 rm -rf $dir
-git switch $new
+git switch $curr
+git stash pop
